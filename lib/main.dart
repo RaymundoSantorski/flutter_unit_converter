9b37cfa0507174aac3544b522bcc6e6820b69e6c
Raymundo Mateos <rayma9829@gmail.com>
2025-09-03 10:57:56 -0600

    add: appbar that shows title

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(appBar: AppBar(title: Text(widget.title)));
   }
 }
