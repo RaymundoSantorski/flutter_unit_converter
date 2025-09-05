11a3feebca155df83cc8a8779461e4c62b3ae174
Raymundo Mateos <rayma9829@gmail.com>
2025-09-05 16:10:26 -0600

    add: tests of Micrometer unit

--- test/length_model_test.dart
+++ test/length_model_test.dart
@@ -67,6 +67,58 @@ void main() {
     });
   });
 
+  /// Test of the Millimeter class, tests convertions from other Length types to Micrometer type
+  group('Micrometer class', () {
+    test('The constructor asigns the correct value', () {
+      final Mm = Micrometer(10.0);
+      expect(Mm.value, 10.0);
+      expect(Mm.type, Micrometer);
+    });
+
+    test('The factory constructor converts from Nanometer to Micrometer', () {
+      final nm = Nanometer(1000); // 1,000nm
+      final Mm = Micrometer.from(nm); // Expects 1Mm
+      expect(Mm.value, 1.0);
+      expect(Mm.type, Micrometer);
+    });
+    test('The factory constructor converts from Micrometer to Micrometer', () {
+      final nm = Micrometer(1000); // 1,000nm
+      final Mm = Micrometer.from(nm); // Expects 1,000Mm
+      expect(Mm.value, 1000.0);
+      expect(Mm.type, Micrometer);
+    });
+    test('The factory constructor converts from Millimeter to Micrometer', () {
+      final mm = Millimeter(0.001); // 0.001mm
+      final Mm = Micrometer.from(mm); // Expects 1Mm
+      expect(Mm.value, 1.0);
+      expect(Mm.type, Micrometer);
+    });
+    test('The factory constructor converts from Centimeter to Micrometer', () {
+      final cm = Centimeter(0.001); // 0.001mm
+      final Mm = Micrometer.from(cm); // Expects 10Mm
+      expect(Mm.value, 10.0);
+      expect(Mm.type, Micrometer);
+    });
+    test('The factory constructor converts from Inch to Micrometer', () {
+      final cm = Inch(1); // 1 in
+      final Mm = Micrometer.from(cm); // Expects 25400Mm
+      expect(Mm.value, 25400.0);
+      expect(Mm.type, Micrometer);
+    });
+    test('The factory constructor converts from Meter to Micrometer', () {
+      final m = Meter(1); // 1m
+      final Mm = Micrometer.from(m); // Expects 1,000,000Mm
+      expect(Mm.value, 1000000);
+      expect(Mm.type, Micrometer);
+    });
+    test('The factory constructor converts from Kilometer to Micrometer', () {
+      final km = Kilometer(1); // 1m
+      final Mm = Micrometer.from(km); // Expects 1,000,000,000Mm
+      expect(Mm.value, 1000000000);
+      expect(Mm.type, Micrometer);
+    });
+  });
+
   /// Test of the Millimeter class, tests convertions from other Length types to Millimeter type
   group('Millimeter class', () {
     test('The constructor asigns the correct value', () {
