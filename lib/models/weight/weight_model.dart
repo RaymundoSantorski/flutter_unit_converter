1bba8e54d65f65b831277fc12c27155e77d53d07
Raymundo Mateos <rayma9829@gmail.com>
2025-09-09 13:52:01 -0600

    update: weight_model support for milligram and gram units

--- lib/models/weight/weight_model.dart
+++ lib/models/weight/weight_model.dart
@@ -1,9 +1,19 @@
+import 'package:flutter_unit_converter/models/weight/gram.dart';
+import 'package:flutter_unit_converter/models/weight/milligram.dart';
+
+List<Weight> units = [Milligram(0), Gram(0)];
+
 abstract class Weight {
   double value;
   Weight(this.value);
-  // factory Weight.from(Type type, double value) {
-  //   return
-  // }
+
+  factory Weight.from(Type type, double value) {
+    switch (type) {
+      case Gram:
+        return Gram(value);
+    }
+    return Milligram(value);
+  }
 
   String get unit => '';
   Type get type => Weight;
